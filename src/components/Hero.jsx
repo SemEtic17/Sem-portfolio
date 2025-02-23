d76bd2c33293e4fa4ee884221303c525c84d4a25
Sem Dori <semetic17@gmail.com>
2025-02-22 22:13:28 -0800

    Fixing typo error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
               Hi, I'm <span className="text-[#915EFF]">Sem</span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop Mern stuck apps, with <br className="sm:block hidden" />
+              I develop MERN stack apps, with <br className="sm:block hidden" />
               CRUD operations
             </p>
           </div>
