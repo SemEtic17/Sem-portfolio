dea83fdefcf8596d410611046ef1c37e3262e1fe
SemEtic17 <semetic17@gmail.com>
2025-07-17 15:00:22 +0300

    update description styling and correct typo in Works component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -55,14 +55,14 @@ const ProjectCard = ({
           >
             {name}
           </h3>
-          <p className="mt-2 text-secondary text-[14px] line-clamp-4">
+          <p className="my-2 text-secondary text-[14px] max-h-32 line-clamp-6">
             {description}
           </p>
         </div>
         <p className="text-yellow-300">
-          visite the website by clicking the title
+          visit the website by clicking the title
         </p>
-        <div className="mt-4 flex flex-wrap gap-2">
+        <div className="mt-1 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
